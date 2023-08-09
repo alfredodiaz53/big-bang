27eaa312618674850a0573d642c48844a782152d
sumathi Rajendran <sumathi.rajendran@omnifederal.com>
2023-08-09 16:50:51 -0400

    updating version 2.8.x

--- venv/lib/python3.10/site-packages/keepachangelog/__init__.py
+++
@@ -1,3 +0,0 @@
-from keepachangelog.version import __version__
-from keepachangelog._changelog import to_dict, to_raw_dict, release, from_dict
-from keepachangelog._versioning import to_sorted_semantic
